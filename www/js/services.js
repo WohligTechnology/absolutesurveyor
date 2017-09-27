26ce02b55503bd88a84a1d9a7941a32dd94c34df
NileshWangad <nilesh.wangad@wohlig.com>
2017-09-27 12:25:51 +0530

    ready to publish

--- www/js/services.js
+++ www/js/services.js
@@ -1,8 +1,8 @@
 // var adminurl = "http://wohlig.io/api/"; //local
 
 // var adminurl = "http://104.198.28.29:80/api/"; //server 
-var adminurl = "http://absolute.wohlig.co.in/api/"; //Test server
-// var adminurl = "http://absolutesurveyors.com/api/"; //server
+// var adminurl = "http://absolute.wohlig.co.in/api/"; //Test server
+var adminurl = "http://absolutesurveyors.com/api/"; //server
 
 // var imgpath = adminurl + "uploadfile/getupload?file=";
 var imgurl = adminurl + "upload/";
